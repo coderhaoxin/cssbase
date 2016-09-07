86f5d5b79730ce817f4cbefd54c05de25ad1b13e
Zha Xinyu <newraina@users.noreply.github.com>
2016-09-07 17:12:03 +0800

    generate rem-375.css
    * generate rem-375.css
    
    * fix rem generator
    
    PR #2

--- rem.js
+++ rem.js
@@ -6,16 +6,16 @@ const fs = require('fs')
 const start = 320
 const end = 420
 
-const bases = [320]
+const bases = [320, 375]
 const range = 10
 
 bases.forEach(base => {
   const file = fs.createWriteStream(path.resolve(`rem-${base}.css`))
   for (let i = start; i < end; i += range) {
-    const d = 16 / base
+    const s = parseFloat((16 / base * i).toFixed(2))
     const v = `
 @media screen and (min-width: ${i}px) and (max-width: ${i + range - 1}px) {
-  html { font-size: ${d * i}px; }
+  html { font-size: ${s}px; }
 }
 `
     file.write(v)
